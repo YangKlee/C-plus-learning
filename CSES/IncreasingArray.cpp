09f8de6a8d453c2345433e46c0cb8bcd62b06c6b
YangKlee <khanhduong18072005@gmail.com>
2024-03-24 21:50:01 +0700

    loli is da best

--- CSES/IncreasingArray.cpp
+++ CSES/IncreasingArray.cpp
@@ -6,57 +6,43 @@ void input_arr(long long a[], int n)
     for (int i = 0; i < n; i++)
     {
         cin >> a[i];
+
     }
     
 }
-void out_arr(long long a[], int n)
+void outarr(long long a[], int n)
 {
     for (int i = 0; i < n; i++)
     {
-        cout << a[i] << " ";
+        cout << a[i] << ' ';
     }
+    cout <<'\n';
     
 }
-bool check_tangdan(long long a[], int n)
+long long move(long long a[], int n)
 {
-    if(n == 0)
-        return 1;
-    else
-    {
-        if(a[n-2] > a[n-1])
-            return 0;
-        else
-            return check_tangdan(a,n-1);
-    }
-}
-int move(long long a[], int n)
-{
-    int min = 0;
-    while(check_tangdan(a,n) == false)
-    {
-        for (int i = 0; i < n-1; i++)
-        {
-            if(a[i] >= a[i+1])
-            {
-                a[i+1] += 1;
+    long long taget = 0;
 
-            }
-            min ++;
+    for (int i = 0; i < n-1; i++)
+    {
+        if(a[i+1] < a[i] )
+        {
+            taget += (a[i]) - a[i+1] ;
+            //cout << "taget = "<< taget << endl;
+            a[i+1] = a[i];
         }
-        //out_arr(a,n);
-        //cout << '\n';
     }
-    return min;
+    //outarr(a,n);
+    return taget;
 
 }
 int main()
 {
-    ios::sync_with_stdio(0);
-    cin.tie(0);
+
     int n;
     cin >> n;
     long long *a = new long long[n];
-    input_arr(a, n);
-    cout << move(a,n);
+    input_arr(a,n);
+    cout << (long long)move(a,n);
 
 }
---
+++ vnoj/hoanvidep.cpp
@@ -0,0 +1,12 @@
+#include <bits\stdc++.h>
+using namespace std;
+int main()
+{
+    int n;
+    vector <int> a;
+    for (int i = 1; i <= n; i++)
+    {
+        a.push_back(i);
+    }
+    
+}
