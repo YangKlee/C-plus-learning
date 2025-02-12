f6a7421e3121e5b30d094e998c67200eacbee6d1
YangKlee <147032564+YangKlee@users.noreply.github.com>
2025-02-12 16:00:46 +0700

    dit me doi

--- CTDL/BTH/1/DynamicArray2.cpp
+++ CTDL/BTH/1/DynamicArray2.cpp
@@ -10,19 +10,50 @@ int* inputDynamicArr(int n)
     }
     return a;
 }
-void displayArr(int *a, int n)
+//Hàm in mảng động n số nguyên có địa chỉ từ arr.
+void printDynamicArr(int *arr, int n)
 {
     for (int i = 0; i < n; i++)
     {
-        cout << *a << " ";
-        a++;
+        cout << arr[i] << " ";
     }
     cout << "\n";
+}
+// Hàm thêm một số x vào cuối mảng arr có n số.
+void appendDynamicArr(int *arr, int &n, int x)
+{
+    int* startArr = arr;
+    int *new_arr = new int[n+1];
+    int *start_newArr = new_arr;
+    for (int i = 0; i < n; i++)
+    {
+        *new_arr = *arr;
+        new_arr++; arr++; 
+    }
+    new_arr = start_newArr;
+    *(new_arr + n) = x;
+    n += 1;
+    arr = new int[n];
+    //printDynamicArr(new_arr, n);
+    arr = startArr;   
+    for (int i = 0; i < n; i++)
+    {
+        *arr = *new_arr;
+        arr++; new_arr++;
+    }
+    arr = startArr;
+    
 }
 int main()
 {
     int n;
+    cout << "Nhap so phan tu mang: ";
     cin >> n;
     int *a = inputDynamicArr(n);
-    displayArr(a,n);
+    cout << "Nhap phan tu de them vao mang: ";
+    int x;
+    cin >> x;
+    appendDynamicArr(a, n, x);
+    cout << "Ket qua: ";
+    printDynamicArr(a, n);
 }
